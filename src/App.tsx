70822efe68c599f6e6a83c6396caf4d6c9c48fe3
heremel <melissaferreira2510@gmail.com>
2025-06-10 09:53:28 +0200

    finalpush

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import EmployeeCard from './components/EmployeeCard';
-import './App.css'
-import { useState } from 'react';
+import EmployeeCard from "./components/EmployeeCard";
+import "./App.css";
+import { useState } from "react";
 
 const sampleEmployee = {
   name: {
@@ -16,20 +16,21 @@ const sampleEmployee = {
 function App() {
   const [employee, setEmployee] = useState(sampleEmployee);
   const getEmployee = () => {
-    fetch ("https://randomuser.me/api?nat=en")
-    .then((response) => response.json())
-    .then((data) => {
-      setEmployee(data.results[0]);
-    });
+    fetch("https://randomuser.me/api?nat=en")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
   };
 
-
   return (
-    <div className='App'>
-     <EmployeeCard employee={employee}/>
-     <button type="button" onClick={getEmployee}/>Get employee<button/>
+    <div className="App">
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>
+        Get employee
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
